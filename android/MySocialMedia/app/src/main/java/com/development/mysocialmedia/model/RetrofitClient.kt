abbeb30961428312518ef8642ffaa4ca66795719
Riihacode <riihacode@gmail.com>
2025-02-07 07:03:00 +0700

    Updated Android and README

--- android/MySocialMedia/app/src/main/java/com/development/mysocialmedia/model/RetrofitClient.kt
+++ android/MySocialMedia/app/src/main/java/com/development/mysocialmedia/model/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.200.17.75:3000/"
+    private const val BASE_URL = "http://192.168.18.39:3000/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply{
         level = HttpLoggingInterceptor.Level.BODY
