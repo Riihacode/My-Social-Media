6d6e99e31be114b15038c8ee8794ec8f2760782c
Riihacode <riihacode@gmail.com>
2025-02-13 03:53:49 +0700

    Updated Android, still figuring out to customize color button of btnAccountLogout

--- android/MySocialMedia/app/src/main/java/com/development/mysocialmedia/view/activity/MainActivity.kt
+++ android/MySocialMedia/app/src/main/java/com/development/mysocialmedia/view/activity/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.development.mysocialmedia.view.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.development.mysocialmedia.R
 import com.development.mysocialmedia.SessionManager
 import com.development.mysocialmedia.databinding.ActivityMainBinding
+import com.development.mysocialmedia.view.fragment.AccountFragment
+import com.development.mysocialmedia.view.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +23,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkSessionUserLogin()
+        setupBottomNavigationListener()
+        // logoutUserLogin()
+    }
+
+    /*
+    private fun logoutUserLogin() {
+        // Tombol logout
+        binding.btnLogout.setOnClickListener {
+            sessionManager.logout()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+     */
+
+    private fun checkSessionUserLogin() {
         sessionManager = SessionManager(this)
         // Cek apakah user sudah login
         if (!sessionManager.isLoggedIn()) {
@@ -34,13 +57,26 @@ class MainActivity : AppCompatActivity() {
                 sessionManager.setFirstLogin(false)
             }
         }
+    }
 
-        // Tombol logout
-        binding.btnLogout.setOnClickListener {
-            sessionManager.logout()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+    private fun setupBottomNavigationListener() {
+        binding.bottomNavigationView.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_account -> replaceFragment(AccountFragment())
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, addToBackStack:Boolean = false) {
+        val transaction = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment)
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
         }
 
+        transaction.commit()
     }
 }
